5f7dd6ad52c3b20ef2c930c7b6d9f257f7d08123
Ilham <utarbaev@iceberg.hockey>
2019-06-11 17:37:01 +0300

    Initial

---
+++ precache-manifest.6af833c35a632547224d48e3668d8611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab272af6e9ba2cdec2ccfa020e391d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf59d17f8dbc952e84f",
+    "url": "/static/css/main.d7a84dc1.chunk.css"
+  },
+  {
+    "revision": "a07c5b204e2d0f0eed59",
+    "url": "/static/js/2.76fc5c5f.chunk.js"
+  },
+  {
+    "revision": "0bf59d17f8dbc952e84f",
+    "url": "/static/js/main.91dd47ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
